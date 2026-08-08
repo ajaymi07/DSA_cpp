81f1f1bec0345f348380818f99d5e900c84377c4
Ajay Kumar Mishra <ajajymishra32@gmail.com>
2026-08-08 16:36:08 +0530

    basic dsa

---
+++ ascii01.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+   char a='B';
+
+   int y=(int)a;
+   cout<<y<<endl; //explicit typecasting
+}
---
+++ ascii02.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main (){
+    char ch1='a';
+    char ch2='b';
+    cout<<ch1+ch2<<endl;
+} 
